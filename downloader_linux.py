7551bc1a0c97d4a3fefaebc50811097cc025989c
Vacheprime <77219495+Vacheprime@users.noreply.github.com>
2021-06-12 11:54:57 -0400

    Add files via upload

---
+++ downloader_linux.py
@@ -0,0 +1,39 @@
+import youtube_dl
+
+action = ""
+options = ""
+while action != "quit":
+	action = input("Enter URL to download or 'quit' to quit: ")
+	options = ""
+	if action != "quit":
+		while not options in ["A", "a", "V", "v", "quit"]:
+			options = input("Audio/video? [A/v] 'quit' to quit: ")
+			if options == "A" or options == "a":
+				ytdl_opts = {"ffmpeg_location":"ffmpeg-git-20210528-amd64-static/ffmpeg", "format":"bestaudio/best",
+				 "postprocessors": [{
+					"key": "FFmpegExtractAudio",
+					"preferredcodec": "mp3"
+					}]}
+				ytdl = youtube_dl.YoutubeDL(ytdl_opts)
+				try:
+					with ytdl as yt:
+						yt.download([action])
+					break
+				except Exception:
+					break
+			elif options == "V" or options == "v":
+				ytdl_opts = {"ffmpeg_location":"ffmpeg-git-20210528-amd64-static/ffmpeg",
+				 "postprocessors": [{
+					"key": "FFmpegVideoConvertor",
+					"preferedformat": "mp4"
+					}]}
+				ytdl = youtube_dl.YoutubeDL(ytdl_opts)
+				try:
+					with ytdl as yt:
+						yt.download([action])
+					break
+				except Exception:
+					break
+			else:
+				if options != "quit":
+					print("Invalid option!")
